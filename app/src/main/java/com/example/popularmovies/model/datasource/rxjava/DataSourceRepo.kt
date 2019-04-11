1a96f8741ef0cd425c16aa7e638d8a6867c85ef4
Nafisisty <nafisisty@gmail.com>
2019-04-11 11:32:25 -0400

    Changed the api key and sort type into constants

--- app/src/main/java/com/example/popularmovies/model/datasource/rxjava/DataSourceRepo.kt
+++ app/src/main/java/com/example/popularmovies/model/datasource/rxjava/DataSourceRepo.kt
@@ -1,5 +1,7 @@
 package com.example.popularmovies.model.datasource.rxjava
 
+import com.example.popularmovies.model.datasource.Constants.Companion.API_KEY
+import com.example.popularmovies.model.datasource.Constants.Companion.SORT_BY
 import com.example.popularmovies.model.datasource.retrofit.RetrofitHelper
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -11,7 +13,7 @@ class DataSourceRepo {
 
     fun getPopularMovie(callback: Callback) {
 
-        retrofitHelper.getMovieOb("1173e8e0543a9fc4b37cac32d448b130", "popularity.desc")
+        retrofitHelper.getMovieOb(API_KEY, SORT_BY)
             .subscribeOn(Schedulers.io())
             .retryWhen { o -> o.take(5).delay(2, TimeUnit.SECONDS) }
             .observeOn(AndroidSchedulers.mainThread())
